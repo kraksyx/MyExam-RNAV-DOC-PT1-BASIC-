9524b52014ed726838af25ee007411f09dfd1208
kraksyx <blahblahusdme@gmail.com>
2025-05-24 20:34:20 +0400

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,73 @@
+
+const questions = [
+  {
+    question: "Decimetric waves correspond to the frequency range:",
+    options: ["3000 to 30000 MHz", "3000 to 30000 KHz", "300 to 3000 MHz", "300 to 3000 KHz"],
+    correct: 2
+  },
+  {
+    question: "An aircraft traveling at 330 meters a second transmits a signal at 10 GHz to a stationary receiver. If the aircraft is flying directly towards the receiver and they are approximately at the same height the received frequency will be:",
+    options: ["11 MHz", "10.000011 GHz", "9.999989 GHz", "11 GHz"],
+    correct: 1
+  },
+  {
+    question: "A half wave dipole aerial suitable for transmitting an RF signal at 18 MHz should have an effective length of:",
+    options: ["16.67 meters", "166.67 meters", "83.33 meters", "8.33 meters"],
+    correct: 3
+  },
+  {
+    question: "Which of the following statements is true?",
+    options: ["A broad bandwidth gives a narrow beam width", "A narrow bandwidth improves beam width", "A transmission's bandwidth is affected by the design of the aerial", "Bandwidth must be reduced in order to reduce noise"],
+    correct: 3
+  },
+  {
+    question: "With regards to radio waves, which statement is true?",
+    options: ["They are reflected by metallic objects with a size compatible to the wavelength", "The longer the wavelength the greater the surface attenuation", "They travel at 186,000 m a second in a vacuum", "High frequencies need large aerials"],
+    correct: 0
+  },
+  {
+    question: "You are at an altitude of 9,000 feet. At a range of 200 nm from a VHF communications transmitter, and you are receiving a good signal.",
+    options: ["You should expect this since the transmitter is at a height of 2,000 feet", "You should have been receiving the signal from a range of 240 nm", "You are probably receiving a sky wave signal", "You are probably receiving a duct propagation signal"],
+    correct: 3
+  },
+  {
+    question: "The emission characteristics A3E describe:",
+    options: ["ILS", "VHF communications", "HF communications", "VOR"],
+    correct: 1
+  },
+  {
+    question: "The gain of an aerial is a measure of its ability to:",
+    options: ["Focus power", "Transmit intelligence", "Overcome transmitter line resistance", "Compensate for attenuation"],
+    correct: 3
+  },
+  {
+    question: "What is the lowest frequency where freedom from static interference can be guaranteed?",
+    options: ["3 MHz", "30 MHz", "300 MHz", "3 GHz"],
+    correct: 1
+  },
+  {
+    question: "The VHF frequency band has a wavelength limit of:",
+    options: ["100 m to 10 m", "1 m to 100 cm", "10 m to 1 m", "100 cm to 10 cm"],
+    correct: 2
+  },
+  {
+    question: "The wavelength of a radio signal is 200 meters. What is the frequency?",
+    options: ["1.5 KHz", "1.5 GHz", "1.5 MHz", "1.3 MHz"],
+    correct: 2
+  },
+  {
+    question: "The approximate ground waves of LF and MF are (by day) ___ and ___ respectively, with ___ suffering more from atmospheric attenuation.",
+    options: ["1500nm; 1000nm; MF", "1000nm; 500nm; LF", "1000nm; 300nm; MF", "500nm; 100nm; LF"],
+    correct: 2
+  },
+  {
+    question: "As the frequency of a transmitter is increased, the range of the ground wave will:",
+    options: ["Decrease", "Decrease only at night", "Increase only over the sea"],
+    correct: 0
+  },
+  {
+    question: "An RMI indicates aircraft heading. To convert the RMI bearings of NDBs and VORs to true bearings the correct combination for the application of magnetic variation is:",
+    options: ["NDB: aircraft position, VOR: beacon position", "NDB: beacon position, VOR: aircraft position", "Both use aircraft position", "Both use beacon position"],
+    correct: 0
+  }
+];
